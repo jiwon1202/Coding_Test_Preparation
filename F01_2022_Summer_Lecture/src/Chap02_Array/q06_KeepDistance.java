e970e91156cc6cabb1c24fc2758e58bd5e6f3ed8
leejiwon1202 <ccap122@skuniv.ac.kr>
2022-08-10 23:46:39 +0900

    거리 두기 - 최대 길이 출력

--- F01_2022_Summer_Lecture/src/Chap02_Array/q06_KeepDistance.java
+++ F01_2022_Summer_Lecture/src/Chap02_Array/q06_KeepDistance.java
@@ -1,14 +1,13 @@
-package main1;
+package Chap02_Array;
 
-import java.util.*;
-
-public class q2_6 {
+public class q06_KeepDistance {
 	public int Solution(int[] nums) {
 		int answer = 0;
 		int n = nums.length;
 		int[] dist = new int[n];
 		
-		int d = 1000;
+		// 현재 좌석에서 왼쪽 방향 기준으로 가장 가까운 사람과의 거리를 배열에 저장
+		int d = 100;
 		for(int i=0; i<n; i++) {
 			if (nums[i] == 1) {
 				d = 0;
@@ -19,6 +18,7 @@ public class q2_6 {
 			}
 		}
 		
+		// 이번엔 배열의 오른쪽부터 거리를 측정하여 더 작은 값으로 배열 값 설정
 		d = 0;
 		for(int i=n-1; i>=0; i--) {
 			if (nums[i] == 1) {
@@ -29,19 +29,15 @@ public class q2_6 {
 				dist[i] = Math.min(dist[i], d);
 			}
 		}
-
 		
 		for (int x : dist) {
 			answer = Math.max(x, answer);
 		}
-		
 		return answer;
 	}
 	public static void main(String[] args) {
-		q2_6 T = new q2_6();
-		int[] arr = new int[] {1, 0, 0, 0, 1, 0 ,0, 1, 0, 1};
-		System.out.println(T.Solution(arr));
-		int[] arr1 = new int[] {0, 0, 0, 0, 0, 0 ,0, 0, 0, 1};
-		System.out.println(T.Solution(arr1));
+		q06_KeepDistance T = new q06_KeepDistance();
+		System.out.println(T.Solution(new int[] {1, 0, 0, 0, 1, 0 ,0, 1, 0, 1}));
+		System.out.println(T.Solution(new int[] {0, 0, 0, 0, 0, 0 ,0, 0, 0, 1}));
 	}
 }

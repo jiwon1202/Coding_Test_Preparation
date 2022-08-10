12f0d077813e6e4562bd04410481d6c0bc3dadb4
leejiwon1202 <ccap122@skuniv.ac.kr>
2022-08-10 21:13:24 +0900

    폴더 삭제

--- F01_2022_Summer_Lecture/src/Chap01_String/q01_StrCompression.java
+++ F01_2022_Summer_Lecture/src/Chap01_String/q01_StrCompression.java
@@ -5,12 +5,15 @@ import java.util.*;
 public class q01_StrCompression {
 	public String solution(String s) {
 		String answer = "";
-		s = s + " ";
+		s = s + " ";	// 공백을 하나 추가해줘야 IndexError를 방지할 수 있음
 		
 		int cnt = 1;
 		for (int i=0; i < s.length() - 1; i++) {
+			// 만약 현재 단어와 그 다음의 단어가 같다면 cnt++
 			if (s.charAt(i) == s.charAt(i+1))
 				cnt++;
+			
+			// 그렇지 않다면 카운팅을 초기화하고 answer 값 수정
 			else {
 				answer += s.charAt(i);
 				if (cnt > 1)
@@ -20,7 +23,7 @@ public class q01_StrCompression {
 		}
 		return answer;
 	}
-	
+	 
 	public static void main(String[] args) {
 		q01_StrCompression T = new q01_StrCompression();
 		System.out.println(T.solution("AAABCCCDD"));

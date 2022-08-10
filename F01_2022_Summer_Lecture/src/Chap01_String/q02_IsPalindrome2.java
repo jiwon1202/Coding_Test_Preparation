0ce67af4746428765ae8bf219fddd691b38e8e3d
leejiwon1202 <ccap122@skuniv.ac.kr>
2022-08-10 21:21:48 +0900

    í쉶臾¸ í뙋蹂 - í룷認ì뼇履½ í룷ì씤í꽣 ì궗ì슜

--- F01_2022_Summer_Lecture/src/Chap01_String/q02_IsPalindrome2.java
+++ F01_2022_Summer_Lecture/src/Chap01_String/q02_IsPalindrome2.java
@@ -1,15 +1,20 @@
-package hi;
+package Chap01_String;
 
 import java.util.*;
 
-public class q2_1 {
+public class q02_IsPalindrome2 {
 	public String solution(String s) {
 		String answer = "YES";
+		
+		// 판별을 위해 모두 대문자로 바꿔줌
 		s = s.toUpperCase();
+		
+		// 양 끝쪽 문자를 가리키는 포인터 두 개 선언
 		int right = s.length()-1;
 		int left = 0;
-			
+		
 		while(left < right) {
+			// 두 포인터가 가리키는 값이 같지 않다면 함수 탈출
 			if (s.charAt(left) != s.charAt(right))
 				return "NO";
 			else {
@@ -21,7 +26,7 @@ public class q2_1 {
 		return answer;
 	}
 	public static void main(String[] args) {
-		q2_1 T = new q2_1();
+		q02_IsPalindrome2 T = new q02_IsPalindrome2();
 		System.out.println(T.solution("gooG"));
 		System.out.println(T.solution("Moon"));
 	}

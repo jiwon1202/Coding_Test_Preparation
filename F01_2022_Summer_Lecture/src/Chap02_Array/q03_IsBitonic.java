19fff6f9bad3980720fa271a646b4def1aabc7c9
leejiwon1202 <ccap122@skuniv.ac.kr>
2022-08-10 23:13:28 +0900

    바이토닉 수열 판별

--- F01_2022_Summer_Lecture/src/Chap02_Array/q03_IsBitonic.java
+++ F01_2022_Summer_Lecture/src/Chap02_Array/q03_IsBitonic.java
@@ -1,31 +1,30 @@
-package main1;
+package Chap02_Array;
 
-import java.util.*;
-
-public class q2_3 {
+public class q03_IsBitonic {
 	public String Solution(int[] nums) {
 		int n = nums.length;
 		int i = 0;
 		
+		// 증가수열이 끝나는 지점의 인덱스 탐색
 		while((i < n-1) && (nums[i] < nums[i+1]))
 			i++;
+		// 증가하지 않았거나 배열의 처음부터 끝까지 증가하기만 한다면 NO
 		if ((i == 0) || (i == n-1))
 			return "NO";
 		
+		// 감소수열이 끝나는 지점의 인덱스 탐색
 		while((i < n-1) && (nums[i] > nums[i+1]))
 			i++;
+		// 배열의 끝까지 도달하지 못했다면 NO / 도달했다면 YES
 		if (i != n-1)
 			return "NO";
-		
-		return "Yes";
+		else
+			return "Yes";
 	}
 	public static void main(String[] args) {
-		q2_3 T = new q2_3();
-		int[] arr = new int[] {1, 2, 3, 4 ,5, 3, 1};
-		System.out.println(T.Solution(arr));
-		int[] arr1 = new int[] {1, 3, 4, 5 ,5, 6, 4, 3};
-		System.out.println(T.Solution(arr1));
-		int[] arr2 = new int[] {1, 2, 3, 4 ,5};
-		System.out.println(T.Solution(arr2));
+		q03_IsBitonic T = new q03_IsBitonic();
+		System.out.println(T.Solution(new int[] {1, 2, 3, 4 ,5, 3, 1}));
+		System.out.println(T.Solution(new int[] {1, 3, 4, 5 ,5, 6, 4, 3}));
+		System.out.println(T.Solution(new int[] {1, 2, 3, 4 ,5}));
 	}
 }

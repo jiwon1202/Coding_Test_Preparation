7d120171cb9d125d59acbabbc2e02af40465cfcd
leejiwon1202 <ccap122@skuniv.ac.kr>
2022-08-10 22:59:35 +0900

    제곱수 정렬

--- F01_2022_Summer_Lecture/src/Chap02_Array/q01_SortSquared.java
+++ F01_2022_Summer_Lecture/src/Chap02_Array/q01_SortSquared.java
@@ -1,8 +1,6 @@
-package main1;
+package Chap02_Array;
 
-import java.util.*;
-
-public class q2_1 {
+public class q01_SortSquared {
 	public int[] Solution(int[] nums) {
 		int n = nums.length;
 		int[] answer = new int[n];
@@ -11,6 +9,8 @@ public class q2_1 {
 		int right = n-1;
 		
 		for (int i=n-1; i>=0; i--){
+			// 절댓값이 크다면 그 값의 제곱을 answer 배열의 오른쪽 끝에 삽입
+			// 그 후 포인터를 옮겨가며 위의 과정 반복
 			if (Math.abs(nums[left]) >= Math.abs(nums[right])) {
 				answer[i] = nums[left] * nums[left];
 				left++;
@@ -23,15 +23,14 @@ public class q2_1 {
 		return answer;
 	}
 	public static void main(String args[]) {
-		q2_1 T = new q2_1();
-		int[] arr = new int[] {-4, -1, 0, 3, 10};
-		for(int n:T.Solution(arr))
+		q01_SortSquared T = new q01_SortSquared();
+		
+		for(int n:T.Solution(new int[] {-4, -1, 0, 3, 10}))
 			System.out.print(n + " ");
 		
 		System.out.println();
 		
-		int[] arr1 = new int[] {-7, -3, 2, 3, 11};
-		for(int n:T.Solution(arr1))
+		for(int n:T.Solution(new int[] {-7, -3, 2, 3, 11}))
 			System.out.print(n + " ");
 	}
 }

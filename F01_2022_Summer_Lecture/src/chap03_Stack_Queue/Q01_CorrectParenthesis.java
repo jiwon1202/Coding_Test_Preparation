ecfb99a4ed6300a33ef9fcfbca7e55c5c773039f
leejiwon1202 <ccap122@skuniv.ac.kr>
2022-08-11 01:26:24 +0900

    chap03 폴더 생성

---
+++ F01_2022_Summer_Lecture/src/chap03_Stack_Queue/Q01_CorrectParenthesis.java
@@ -0,0 +1,13 @@
+package chap03_Stack_Queue;
+
+public class Q01_CorrectParenthesis {
+	public String solution(String s) {
+		String answer = "YES";
+		
+		return answer;
+	}
+	public static void main(String[] args) {
+		Q01_CorrectParenthesis T = new Q01_CorrectParenthesis();
+		System.out.println(T.solution("(()(()))(()"));
+	}
+}

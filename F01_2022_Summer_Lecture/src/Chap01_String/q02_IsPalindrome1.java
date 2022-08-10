016a52779cd0696901b5c53c7f2b93996fcb9825
leejiwon1202 <ccap122@skuniv.ac.kr>
2022-08-10 21:19:01 +0900

    회문 판별 - 라이브러리 함수 사용

--- F01_2022_Summer_Lecture/src/Chap01_String/q02_IsPalindrome1.java
+++ F01_2022_Summer_Lecture/src/Chap01_String/q02_IsPalindrome1.java
@@ -1,19 +1,19 @@
-package hi;
+package Chap01_String;
 
 import java.util.*;
 
-public class q2 {
+public class q02_IsPalindrome1 {
 	public String solution(String s) {
 		String answer = "NO";
-		String tmp = new StringBuilder(s).reverse().toString();	// 문자열을 뒤집음
+		String tmp = new StringBuilder(s).reverse().toString();
 		
-		if (s.equalsIgnoreCase(tmp))	// 대소문자 상관없이 비교하여 동일한 문자열이라면 "YES"
+		if (s.equalsIgnoreCase(tmp))
 			answer = "YES";
 		
 		return answer;
 	}
 	public static void main(String[] args) {
-		q2 T = new q2();
+		q02_IsPalindrome1 T = new q02_IsPalindrome1();
 		System.out.println(T.solution("gooG"));
 		System.out.println(T.solution("Moon"));
 	}

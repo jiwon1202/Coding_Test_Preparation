8380786836f1472dc1f3f471265b7aab86109e53
leejiwon1202 <ccap122@skuniv.ac.kr>
2022-08-10 23:28:39 +0900

    최대 길이 바이토닉 수열 탐색

--- F01_2022_Summer_Lecture/src/Chap02_Array/q04_MaximumBitonic.java
+++ F01_2022_Summer_Lecture/src/Chap02_Array/q04_MaximumBitonic.java
@@ -1,42 +1,41 @@
-package main1;
+package Chap02_Array;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-public class q2_4 {
+public class q04_MaximumBitonic {
 	public int Solution(int[] nums) {
 		int answer = 0;
 		int n = nums.length;
 		
+		// 바이토닉 수열의 최댓값, 즉 꼭짓점의 인덱스를 peaks에 저장
 		ArrayList<Integer> peaks = new ArrayList<>();
 		for (int i=1; i<n-1; i++) {
 			if ((nums[i] > nums[i-1]) && (nums[i] > nums[i+1]))
 				peaks.add(i);
 		}
 		
+		// 각 꼭짓점을 기준으로 양 옆으로 뻗어나가며 바이토닉 수열의 최대 길이 탐색
 		for (int p : peaks) {
 			int left = p;
 			int right = p;
 			int cnt = 1;
 			
-			while (left-1>=0 && nums[left-1] < nums[left]) {
+			while ((0 <= left-1) && (nums[left-1] < nums[left])) {
 				cnt++;
 				left--;
 			}
-			while (right+1<n && nums[right] > nums[right+1]) {
+			while ((right < n-1) && (nums[right] > nums[right+1])) {
 				cnt++;
 				right++;
 			}
-			
 			answer = Math.max(answer, cnt);
 		}
 		return answer;
 	}
 	public static void main(String[] args) {
-		q2_4 T = new q2_4();
-		int[] arr = new int[] {1, 3, 2, 5, 7 ,4, 2, 5, 1};
-		System.out.println(T.Solution(arr));
+		q04_MaximumBitonic T = new q04_MaximumBitonic();
+		System.out.println(T.Solution(new int[] {1, 3, 2, 5, 7 ,4, 2, 5, 1}));
 	}
-
 }

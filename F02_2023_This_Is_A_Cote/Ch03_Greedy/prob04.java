c67563fbcc1988d843275bfe634bd1f347659275
leejiwon1202 <ccap122@skuniv.ac.kr>
2023-03-10 23:34:58 +0900

    230310 p4 수정

--- F02_2023_This_Is_A_Cote/Ch03_Greedy/prob04.java
+++ F02_2023_This_Is_A_Cote/Ch03_Greedy/prob04.java
@@ -1,10 +1,34 @@
 package Chap03_Greedy;
 
+import java.util.Scanner;
+
 public class prob04 {
+	public int solution(int n, int k) {
+		int answer = 0;
+		
+		while(true) {
+			int tmp = (int)(n / k) * k;
+			answer += (n - tmp);
+			n = tmp;
+			
+			if (n < k)
+				break;
+			
+			answer++;
+			n /= k;
+		}
+		
+		return answer + n - 1;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		prob04 T = new prob04();
+		Scanner kb = new Scanner(System.in);
 
+		int n = kb.nextInt();
+		int k = kb.nextInt();
+		
+		System.out.println(T.solution(n, k));
 	}
 
 }
